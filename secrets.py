cb2cee81a363fa7a3f57b4dd425a46f30d950874
Nish Gowda <60044986+nishgowda@users.noreply.github.com>
2020-03-23 22:58:37 -0400

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,5 @@
+# Make sure to fill in your spotify client_secret information
+spotify_token = "BQBc9SKqb_GmR-P1AZuhTqf-TAOjch9hTMD3enGXlQKv5AXMSsaTgZG6SCsh" \
+                "-WFfN3GHN5mz7j4v0KGM401c0qdKh8PJzQNqBe2dsySxbWwmDaOfuBVznn2H-dC_jwJ1porXbTnfCWfoe-4EdYR_2QOr5llFsf" \
+                "-ETT9oNwCHWyPjz9w "
+spotify_user_id = "nishman606"
