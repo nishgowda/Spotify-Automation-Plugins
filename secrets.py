d405153caedb1d43879ae6ec126f470546eda33b
Nish Gowda <60044986+nishgowda@users.noreply.github.com>
2020-03-25 16:46:28 -0400

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,5 +1,3 @@
 # Make sure to fill in your spotify client_secret information
-spotify_token = "BQBc9SKqb_GmR-P1AZuhTqf-TAOjch9hTMD3enGXlQKv5AXMSsaTgZG6SCsh" \
-                "-WFfN3GHN5mz7j4v0KGM401c0qdKh8PJzQNqBe2dsySxbWwmDaOfuBVznn2H-dC_jwJ1porXbTnfCWfoe-4EdYR_2QOr5llFsf" \
-                "-ETT9oNwCHWyPjz9w "
-spotify_user_id = "nishman606"
+spotify_token = "#YOUR API KEY "
+spotify_user_id = "#YOUR SPOTIFY ID"
